00c268f420079ba5d0b6931570514282037131ec
LKANIT Mohamed Taib <mohamed.lkanit@um6p.ma>
2025-04-19 23:02:39 +0100

    nothing interesting

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="profile-container">
+    <div class="profile-container" v-if="!editMode">
       <div v-if="!infos" class="loading-container">
         <div class="loading-spinner"></div>
         <div class="loading-text">Loading your profile...</div>
@@ -10,7 +10,7 @@
           <div class="profile-avatar-container">
             <img :src="infos.imgURL" alt="Profile picture" class="profile-avatar" />
           </div>
-          <div class="settings-button" v-if="auth.currentUser.uid == $route.params.id">
+          <div class="settings-button" v-if="auth.currentUser.uid == $route.params.id" @click="editMode = !editMode">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <circle cx="12" cy="12" r="3"></circle>
               <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
@@ -73,6 +73,10 @@
         </div>
       </div>
     </div>
+
+    <div v-else>
+       
+    </div>
   </template>
   
   <script setup>
@@ -81,6 +85,9 @@
   
   // Variables
   const infos = ref(null);
+  const editMode = ref(false)
+  const name = ref("")
+  const username = ref("")
   
   // Functions
   onMounted(async () => {
